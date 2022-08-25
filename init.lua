fe928dfc24ba92b60c174ae8b001a4d46664aa4c
Fredrik Fossum <fredrik.fossum@finn.no>
2022-08-25 17:00:49 +0200

    add metals

--- init.lua
+++ init.lua
@@ -90,4 +90,34 @@ return require('packer').startup(function(use)
     map("n", "<leader>t", "<cmd>ToggleTerm<cr>")
     map("t", "<Esc>", "<cmd>ToggleTerm<cr>")
 
+    use({
+        "scalameta/nvim-metals",
+        requires = {"nvim-lua/plenary.nvim"}
+    })
+    vim.opt_global.shortmess:remove("F"):append("c")
+    local metals_config = require("metals").bare_config()
+
+    -- *READ THIS*
+    -- I *highly* recommend setting statusBarProvider to true, however if you do,
+    -- you *have* to have a setting to display this in your statusline or else
+    -- you'll not see any messages from metals. There is more info in the help
+    -- docs about this
+    -- metals_config.init_options.statusBarProvider = "on"
+
+    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", {
+        clear = true
+    })
+    vim.api.nvim_create_autocmd("FileType", {
+        -- NOTE: You may or may not want java included here. You will need it if you
+        -- want basic Java support but it may also conflict if you are using
+        -- something like nvim-jdtls which also works on a java filetype autocmd.
+        pattern = {"scala", "sbt", "java"},
+        callback = function()
+            require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group
+    })
+    map("n", "<leader>mc", '<cmd>lua require"telescope".extensions.metals.commands()<cr>')
+    map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
 end)
