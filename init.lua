5e0c5bb1d5f7b65592296f7ac5e586dc2b642d70
Fredrik Fossum <fredrik.fossum@finn.no>
2023-03-08 14:15:06 +0100

    update deprecated nvimtree setup function

--- init.lua
+++ init.lua
@@ -42,7 +42,6 @@ return require('packer').startup(function(use)
         tag = 'nightly',
         config = function()
             require("nvim-tree").setup {
-                open_on_setup = true,
                 view = {
                     adaptive_size = true
                 },
@@ -51,6 +50,26 @@ return require('packer').startup(function(use)
                     symlink_destination = false
                 }
             }
+            local function open_nvim_tree(data)
+                -- buffer is a [No Name]
+                local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+                -- buffer is a directory
+                local directory = vim.fn.isdirectory(data.file) == 1
+
+                if not no_name and not directory then
+                  return
+                end
+
+                -- change to the directory
+                if directory then
+                  vim.cmd.cd(data.file)
+                end
+
+                -- open the tree
+                require("nvim-tree.api").tree.open()
+            end
+            vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
         end
     }
     map("n", "<leader>tf", "<cmd>NvimTreeFindFile<CR>")
